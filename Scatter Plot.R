f206ef1dcbb9895d8397bf61285d92d8c2ce7ac1
krishnakesari <bkesari@outlook.com>
2020-07-10 07:25:59 -0400

    Scatter Plot

---
+++ Scatter Plot.R
@@ -0,0 +1,14 @@
+# Load the plotly package
+library(plotly)
+
+# Store the scatterplot of Critic_Score vs. NA_Sales sales in 2016
+scatter <- vgsales %>%
+			filter(Year == 2016) %>%
+			ggplot(aes(x = NA_Sales, y = Critic_Score)) +
+			geom_point(alpha = 0.3)
+
+# Convert the scatterplot to a plotly graphic
+ggplotly(scatter)
+
+
+# Plotting a single variable
