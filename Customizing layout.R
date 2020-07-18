09a8ae533f3a3a1c5d813450d51efd1c97c3e5d7
krishnakesari <bkesari@outlook.com>
2020-07-18 07:27:26 -0400

    Layout

--- Customizing layout.R
+++ Customizing layout.R
@@ -4,3 +4,9 @@ vgsales2016 %>%
   add_markers(marker = list(opacity = 0.5)) %>%
   layout(xaxis = list(title = 'Global sales (millions of units)', type = 'log'),
          yaxis = list(title = 'Critic score'))
+
+# Set the background color to #ebebeb and remove the vertical grid
+annual_vgsales %>%
+  plot_ly(x = ~Year, y = ~Global_Sales) %>%
+  add_lines() %>%
+  layout(paper_bgcolor = "#ebebeb", xaxis = list(showgrid = FALSE))
