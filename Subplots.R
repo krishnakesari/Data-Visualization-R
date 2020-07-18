517b2e437b6cbbb06c18781c9734fc1e23354df5
krishnakesari <bkesari@outlook.com>
2020-07-18 08:26:52 -0400

    Subplots

--- Subplots.R
+++ Subplots.R
@@ -13,3 +13,22 @@ p2 <- vgsales2016 %>%
 # Create a facted scatterplot containing p1 and p2
 subplot(p1, p2, nrows = 2)
 
+
+#######################
+
+# Automated Faceting
+# Create a faceted scatterplot of User_Score vs. Critic_Score with 3 rows
+vgsales2016 %>%
+  group_by(Platform) %>%
+  do(
+    plot = plot_ly(data = ., x = ~Critic_Score, y = ~User_Score) %>%
+      add_markers(name = ~Platform)
+  ) %>%
+  subplot(nrows = 3, shareY = TRUE, shareX = TRUE)
+
+  #########################
+
+  # Building shared axis
+  # Add x-axis and y-axis labels, and a title
+subplot(p1, p2, nrows = 2, shareY = TRUE, shareX = TRUE) %>%
+   layout(title = "User score vs. critic score by platform, 2016")
