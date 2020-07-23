a82267eeb4dcbb834d933cc0e5259a4ef6083551
krishnakesari <bkesari@outlook.com>
2020-07-22 20:32:05 -0400

    Advanced maps in plotly

--- Polygons to maps.R
+++ Polygons to maps.R
@@ -22,3 +22,42 @@ senate_map %>%
           colors = c("dodgerblue", "mediumseagreen", "tomato")) %>%
   add_polygons(line = list(width = 0.4, color = toRGB("gray60")), showlegend = FALSE) %>%
   layout(xaxis = map_axes, yaxis = map_axes)
+
+  ######### County level choropleth map ###########
+  
+  # Join the fl_boundaries and
+# fl_results data frames
+senate_vote <- left_join(fl_boundaries, fl_results, by = c("subregion" = "CountyName"))
+
+# Join the fl_boundaries and fl_results data frames
+senate_vote <- left_join(fl_boundaries, fl_results, by = c("subregion" = "CountyName"))
+
+# Create a county-level choropleth map of Pctvote
+senate_vote %>%
+  group_by(group) %>%
+  plot_ly(x = ~long, y = ~lat, 
+          color = ~Pctvote,
+          split = ~subregion) %>%
+  add_polygons(line = list(width = 0.4), showlegend = FALSE, colors = c("blue", "red"))
+
+
+  # Join the fl_boundaries and fl_results data frames
+senate_vote <- left_join(fl_boundaries, fl_results, by = c("subregion" = "CountyName"))
+
+# Specify the axis settings to polish the map
+map_axes <- list(title = "", 
+                 gridlines = FALSE,
+                 zerolines = FALSE,
+                 tickmarks = FALSE)
+
+# Create a polished county-level choropleth map of Pctvote
+senate_vote %>%
+  group_by(group) %>%
+  plot_ly(x = ~long, y = ~lat, 
+          color = ~Pctvote,
+          split = ~subregion) %>%
+  add_polygons(line = list(width = 0.4), showlegend = FALSE, colors = c("blue", "red")) %>%
+  layout(xaxis = map_axes, yaxis = map_axes)
+
+  
+
